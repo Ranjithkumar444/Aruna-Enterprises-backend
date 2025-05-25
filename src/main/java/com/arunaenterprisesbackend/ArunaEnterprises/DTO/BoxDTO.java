d60114542c5595bfa2b31fa8dc0793c07a2b592d
Ranjithkumar444 <ranjithrockzz289@gmail.com>
2025-05-25 14:14:22 +0530

    Box added

---
+++ src/main/java/com/arunaenterprisesbackend/ArunaEnterprises/DTO/BoxDTO.java
@@ -0,0 +1,15 @@
+package com.arunaenterprisesbackend.ArunaEnterprises.DTO;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class BoxDTO {
+    private String boxType;
+    private String box;
+    private String boxDescription;
+    private String boxUrl;
+}
---
+++ src/main/java/com/arunaenterprisesbackend/ArunaEnterprises/Repository/BoxRepository.java
@@ -0,0 +1,8 @@
+package com.arunaenterprisesbackend.ArunaEnterprises.Repository;
+
+import com.arunaenterprisesbackend.ArunaEnterprises.Entity.Box;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoxRepository extends JpaRepository<Box,Long> {
+
+}
