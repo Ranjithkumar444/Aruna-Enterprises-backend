255a1ca18fcff21828adec4041467b4b93697eea
Ranjith Kumar <121955754+Ranjithkumar444@users.noreply.github.com>
2025-06-04 15:50:52 +0530

    Update SecurityConfig.java

--- src/main/java/com/arunaenterprisesbackend/ArunaEnterprises/Config/SecurityConfig.java
+++ src/main/java/com/arunaenterprisesbackend/ArunaEnterprises/Config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of(allowedOrigin));
+        config.setAllowedOrigins(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
